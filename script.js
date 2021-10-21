1caf314e106c43c28425132a0fff002f50071e1e
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-10-21 01:30:28 -0400

    added confirmation windows

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-// Assignment code here
+debugger;
 
+// Assignment code here
+let lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let specials = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~"];
+
+
+function generatePassword() {
+  lowercaseCharacters = false;
+  uppercaseCharacters = false;
+  numberCharacters = false;
+  specialCharacters = false;
+
+  while (!lowercaseCharacters && !uppercaseCharacters && !numberCharacters && !specialCharacters) {
+    let lowercaseCharacters = confirm("Lowercase?");
+    let uppercaseCharacters = confirm("Uppercase?");
+    let numberCharacters = confirm("Numbers?");
+    let specialCharacters = confirm("Special?");
+
+    if (lowercaseCharacters || uppercaseCharacters || numberCharacters || specialCharacters) {
+      break;
+    }
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
