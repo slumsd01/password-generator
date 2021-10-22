f93dc2f09b9e4fd113f2ba41c2c8201643ddda61
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-10-22 14:31:27 -0400

    mvp met

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 debugger;
 
 // Assignment code here
+let passwordCharacters = [];
 let lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specials = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~"];
-
+let emptyPassword = "";
 
 function generatePassword() {
+  let passwordLength = window.prompt("Enter the desired password length.");
+   if (passwordLength <= 8 || passwordLength >= 128) {
+     alert("Please enter a number between 8 and 128.")
+     return generatePassword();
+   }
+
   lowercaseCharacters = false;
   uppercaseCharacters = false;
   numberCharacters = false;
   specialCharacters = false;
 
   while (!lowercaseCharacters && !uppercaseCharacters && !numberCharacters && !specialCharacters) {
-    let lowercaseCharacters = confirm("Lowercase?");
-    let uppercaseCharacters = confirm("Uppercase?");
-    let numberCharacters = confirm("Numbers?");
-    let specialCharacters = confirm("Special?");
+    lowercaseCharacters = confirm("Lowercase");
+    uppercaseCharacters = confirm("Uppercase?");
+    numberCharacters = confirm("Numbers?");
+    specialCharacters = confirm("Special?");
 
     if (lowercaseCharacters || uppercaseCharacters || numberCharacters || specialCharacters) {
       break;
     }
   }
+
+  if (lowercaseCharacters) {
+    passwordCharacters = passwordCharacters.concat(lowercaseLetters);
+  }
+
+  if (uppercaseCharacters) {
+    passwordCharacters = passwordCharacters.concat(uppercaseLetters);
+  }
+
+  if (numberCharacters) {
+    passwordCharacters = passwordCharacters.concat(numbers);
+  }
+
+  if (specialCharacters) {
+    passwordCharacters = passwordCharacters.concat(specials);
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    const random = Math.floor(Math.random() * passwordCharacters.length);
+    emptyPassword += passwordCharacters[random];
+  }
+  
+  return emptyPassword;
 }
 
 // Get references to the #generate element
