4660db550dfb53189ffe5b48bd3a5820db740c31
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-10-22 14:48:45 -0400

    confirmed length criteria & fixed prompt wording

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-debugger;
-
 // Assignment code here
-let passwordCharacters = [];
 let lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specials = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~"];
-let emptyPassword = "";
+let passwordCharacters = [];
+let passwordResult = "";
 
 function generatePassword() {
   let passwordLength = window.prompt("Enter the desired password length.");
-   if (passwordLength <= 8 || passwordLength >= 128) {
+   if (passwordLength <= 7 || passwordLength >= 129) {
      alert("Please enter a number between 8 and 128.")
      return generatePassword();
    }
@@ -21,10 +19,10 @@ function generatePassword() {
   specialCharacters = false;
 
   while (!lowercaseCharacters && !uppercaseCharacters && !numberCharacters && !specialCharacters) {
-    lowercaseCharacters = confirm("Lowercase");
-    uppercaseCharacters = confirm("Uppercase?");
-    numberCharacters = confirm("Numbers?");
-    specialCharacters = confirm("Special?");
+    lowercaseCharacters = confirm("Would you like to include lowercase letters?");
+    uppercaseCharacters = confirm("Would you like to include uppercase letters?");
+    numberCharacters = confirm("Would you like to include number characters?");
+    specialCharacters = confirm("Would you like to include special characters?");
 
     if (lowercaseCharacters || uppercaseCharacters || numberCharacters || specialCharacters) {
       break;
@@ -49,10 +47,9 @@ function generatePassword() {
 
   for (let i = 0; i < passwordLength; i++) {
     const random = Math.floor(Math.random() * passwordCharacters.length);
-    emptyPassword += passwordCharacters[random];
+    passwordResult += passwordCharacters[random];
   }
-  
-  return emptyPassword;
+  return passwordResult;
 }
 
 // Get references to the #generate element
@@ -64,7 +61,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
